7de0e9ccbe9309a9e262e1be5e1fdf029e990ad5
fullheart2 <58101448+fullheart2@users.noreply.github.com>
2019-11-23 01:40:41 -0500

    Update Printer.rb

--- Printer.rb
+++ Printer.rb
@@ -20,3 +20,46 @@ class TwitchBot
   def write_to_system(message)
     @socket.puts message
   end
+
+  def write_to_chat(message)
+    write_to_system "PRIVMSG ##{@channel} :#{message}"
+  end
+
+  def run
+    until @socket.eof? do
+      message = @socket.gets
+      puts message
+
+      if message.match(/^PING :(.*)$/)
+        write_to_system "PONG #{$~[1]}"
+        next
+      end
+
+      if message.match(/PRIVMSG ##{@channel} :(.*)$/)
+        content = $~[1]
+        username = message.match(/@(.*).tmi.twitch.tv/)[1]
+
+        # if content.include? "coffee"
+        #   write_to_chat("PUT THAT COFFEE DOWN!!")
+        # end
+
+        command = <<-OSASCRIPT
+        osascript -e 'display notification "#{content}" with title "#{username}"'
+        OSASCRIPT
+
+        system(command)
+      end
+    end
+  end
+
+  def quit
+    write_to_chat "AlwaysBotCoding is out"
+    write_to_system "PART ##{@channel}"
+    write_to_system "QUIT"
+  end
+
+end
+
+bot = TwitchBot.new
+trap("INT") { bot.quit }
+bot.run
